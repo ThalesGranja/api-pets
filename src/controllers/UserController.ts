e7df681703850560457995edb2c7aa736e405e5f
Thales Granja <thalesgranja.dev@gmail.com>
2025-11-27 02:35:55 -0300

    feat: adicionando validacoes de password e confirmpassword

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -168,21 +168,38 @@ export const UserController = {
       return;
     }
 
+    user.email = email;
+
     if (!phone) {
       res.status(422).json({ message: 'O telefone é obrigatório' });
       return
     }
-    if (!password) {
-      res.status(422).json({ message: 'A senha é obrigatória' });
-      return
-    }
-    if (!confirmpassword) {
-      res.status(422).json({ message: 'A confirmação de senha é obrigatória' });
-      return
-    }
+
+    user.phone = phone;
 
     if (password !== confirmpassword) {
-      res.status(422).json({ message: 'A senha e a confirmação de senha devem ser iguais!' })
+      res.status(422).json({ message: 'As senhas não conferem!' })
+      return
+    } else if (password === confirmpassword && password != null) {
+      // create password
+      const salt = await bcrypt.genSalt(12);
+      const passwordHash = await bcrypt.hash(password, salt);
+
+      user.password = passwordHash;
+    }
+
+    try {
+      // returns user updated data
+      const updatedUser = await User.findOneAndUpdate(
+        { _id: user._id },
+        { $set: user },
+        { new: true },
+      )
+
+      res.status(200).json({ message: "Usuário atualizado com sucesso!" });
+
+    } catch (error) {
+      res.status(500).json({ message: error });
       return
     }
 
